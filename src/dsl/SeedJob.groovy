b07c60056864e601f71ec9625ef7c118059e0616
dminchuk <dminchuk@exadel.com>
2020-03-11 13:30:41 +0300

    adding .xml job parameters

--- src/dsl/SeedJob.groovy
+++ src/dsl/SeedJob.groovy
@@ -4,25 +4,30 @@ import jenkins.model.*
 // defining repository
 def repoName = 'pipeline-demo'
 def seedJobName = 'SeedJob' // Need to be excluded from deletion list
-def enableVnc = 'true'
 def repoPath = 'git://github.com/dmitry-minchuk/' + repoName + '.git'
+
+//jenkins job parameters
+def suiteName = 'suiteName'
+def enableVnc = 'enableVNC'
+def jenkinsDefaultRetryCount = 'jenkinsDefaultRetryCount'
 
 disableScriptApproval()
 cloneRepo(repoPath)
-def xmlFileNames = getXmlFileList(repoName)
-updateJobList(xmlFileNames, seedJobName)
+def xmlFiles = getXmlFileList(repoName)
+updateJobList(xmlFiles, seedJobName)
 
 // creating pipeline jobs
-xmlFileNames.each { fileName ->
-    pipelineJob(fileName) {
+xmlFiles.each { xmlFile ->
+    pipelineJob(xmlFile.getKey()) {
         parameters {
-            globalVariableParam('SUITE_NAME', fileName, 'Suite name')
 //            globalVariableParam('JENKINS_LOCAL_HOST', hostName, 'Host IP')
-            globalVariableParam('ENABLE_VNC', enableVnc, 'VNC')
+            globalVariableParam(suiteName, xmlFile.getKey(), 'Suite name')
+            globalVariableParam(enableVnc, retrieveFileRawValue(xmlFile.getValue() as File, enableVnc), 'Video streaming from selenoid_ui.')
+            globalVariableParam(jenkinsDefaultRetryCount, retrieveFileRawValue(xmlFile.getValue() as File, jenkinsDefaultRetryCount), 'Number of attempts for UI failing test.')
         }
 
         triggers {}
-        description(fileName + '.xml pipeline job')
+        description(xmlFile.getKey() + '.xml pipeline job')
 
         definition {
             cpsScm {
@@ -60,16 +65,15 @@ def getHostIp() {
 
 def getXmlFileList(repoName) {
     // collecting .xml suites
-    def xmlFileNames = []
+    def xmlFiles = [:]
     def dir = new File(repoName + '/src/test/resources/testng_suites/')
     dir.eachFile () { file ->
-        fileName = file.getName()
-        xmlFileNames.add(fileName.substring(0, fileName.lastIndexOf('.')))
+        xmlFiles.put(file.getName(), file)
     }
-    return xmlFileNames
+    return xmlFiles
 }
 
-def updateJobList(xmlFileNames, seedJobName) {
+def updateJobList(xmlFiles, seedJobName) {
     // Getting all registered jobs
     println('=====================================')
 
@@ -82,14 +86,15 @@ def updateJobList(xmlFileNames, seedJobName) {
 
     println('=====================================')
 
-    xmlFileNames.each { x ->
-        println('Found existing xml suites: ' + x)
+    def xmlFileNames = []
+    xmlFiles.each { x ->
+        println('Found existing xml suites: ' + x.getKey())
+        xmlFileNames.add(x.getKey())
     }
 
     println('=====================================')
 
     def jobsToDelete = []
-
     existingJobNames.each { e ->
         if(!xmlFileNames.contains(e) && e != seedJobName) {
             jobsToDelete.add(e)
@@ -104,4 +109,14 @@ def updateJobList(xmlFileNames, seedJobName) {
     }
 
     println('=====================================')
+}
+
+def retrieveFileRawValue(File file, String parameterName) {
+    def splitFile = ""
+    if (file.text.length() > 0) {
+        splitFile = file.text.split("<")
+    }
+    def parameterValue = splitFile.find { it.toString().contains(parameterName)}.toString()
+    println(parameterName + ': ' + parameterValue)
+    return parameterValue
 }
--- src/pipeline/Jenkinsfile.groovy
+++ src/pipeline/Jenkinsfile.groovy
@@ -1,3 +1,8 @@
+//jenkins job parameters
+def suiteName = 'suiteName'
+def enableVnc = 'enableVNC'
+def jenkinsDefaultRetryCount = 'jenkinsDefaultRetryCount'
+
 pipeline {
   agent { label 'master' }
   stages {
@@ -5,9 +10,9 @@ pipeline {
       steps {
         def JENKINS_LOCAL_HOST = sh(script: 'curl http://checkip.amazonaws.com', returnStdout: true)
         sh "mvn clean test " +
-                "-Dsuite=" + '${SUITE_NAME} ' +
+                "-Dsuite=" + '${' + suiteName + '} ' +
                 "-Dselenium_host=http://" + JENKINS_LOCAL_HOST + ":4444/wd/hub " +
-                "-DenableVNC=" + '${ENABLE_VNC}'
+                "-DenableVNC=" + '${' + enableVnc + '} '
       }
     }
 
